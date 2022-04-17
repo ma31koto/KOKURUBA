260d769bf79a82717609b7598c090eeda5cc7e3b
ma31koto <ma31kn@gmail.com>
2022-04-17 13:51:19 +0000

    fix index_ranking

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,18 +7,28 @@ class Public::PostsController < ApplicationController
 
   def index
     @q = Post.ransack(params[:q])
-    @posts = @q.result(distinct: true).page(params[:page]).per(8)
+    @posts = @q.result(distinct: true)
     if params[:confession_ranking] == 'asc' || params[:confession_ranking] == 'desc'
-      @posts = Post.avg_confession_result_ranking(params[:confession_ranking],@posts)
+      @posts_pre = Post.avg_confession_result_ranking(params[:confession_ranking],@posts)
+      @posts = Kaminari.paginate_array(@posts_pre).page(params[:page]).per(8)
     elsif params[:all_rate_ranking] == 'asc' || params[:all_rate_ranking] == 'desc'
-      @posts = Post.avg_all_rate_ranking(params[:all_rate_ranking],@posts)
+      @posts_pre = Post.avg_all_rate_ranking(params[:all_rate_ranking],@posts)
+      @posts = Kaminari.paginate_array(@posts_pre).page(params[:page]).per(8)
     elsif params[:atmosphere_rate_ranking] == 'asc' || params[:atmosphere_rate_ranking] == 'desc'
-      @posts = Post.avg_atmosphere_rate_ranking(params[:atmosphere_rate_ranking],@posts)
+      @posts_pre = Post.avg_atmosphere_rate_ranking(params[:atmosphere_rate_ranking],@posts)
+      @posts = Kaminari.paginate_array(@posts_pre).page(params[:page]).per(8)
     elsif params[:few_people_rate_ranking] == 'asc' || params[:few_people_rate_ranking] == 'desc'
-      @posts = Post.avg_few_people_rate_ranking(params[:few_people_rate_ranking],@posts)
+      @posts_pre = Post.avg_few_people_rate_ranking(params[:few_people_rate_ranking],@posts)
+      @posts = Kaminari.paginate_array(@posts_pre).page(params[:page]).per(8)
     elsif params[:standard_rate_ranking] == 'asc' || params[:standard_rate_ranking] == 'desc'
-      @posts = Post.avg_standard_rate_ranking(params[:standard_rate_ranking],@posts)
+      @posts_pre = Post.avg_standard_rate_ranking(params[:standard_rate_ranking],@posts)
+      @posts = Kaminari.paginate_array(@posts_pre).page(params[:page]).per(8)
+    else
+      @q = Post.ransack(params[:q])
+      @posts = @q.result(distinct: true).page(params[:page]).per(8)
     end
+
+
   end
 
   def show
